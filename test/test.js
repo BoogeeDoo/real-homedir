e72790088b9901522ca2e08b4439f07ff5d41ff6
XadillaX <admin@xcoder.in>
2016-03-05 00:35:06 +0800

    test for ci

--- test/test.js
+++ test/test.js
@@ -6,5 +6,26 @@
  */
 "use strict";
 
-var homedir = require("../");
-console.log(homedir());
+var syncRunner = require("sync-runner");
+
+describe(function() {
+    var homedir = require("../");
+    
+    it("should equal to $HOME and os", function() {
+        var hd = homedir();
+        hd.should.be.eql(process.env.HOME);
+        
+        var os = require("os");
+        if(undefined !== os.homedir) {
+            hd.should.be.eql(require("os").homedir());
+        }
+    });
+
+    it("should equal to another user", function() {
+        if(process.env.TRAVIS) {
+            syncRunner("useradd -m test");
+            var result = syncRunner("sudo -u test node test/test_script.js");
+            result.should.be.eql("/home/test");
+        }
+    });
+});
