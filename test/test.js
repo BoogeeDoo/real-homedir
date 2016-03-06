24498370951057787308d7369ad83b62cee23fa8
XadillaX <admin@xcoder.in>
2016-03-06 22:26:02 +0800

    update for Travis-CI

--- test/test.js
+++ test/test.js
@@ -10,11 +10,11 @@ require("should");
 
 describe("homedir", function() {
     var homedir = require("../");
-    
+
     it("should equal to $HOME and os", function() {
         var hd = homedir();
         hd.should.be.eql(process.env.HOME);
-        
+
         var os = require("os");
         if(undefined !== os.homedir) {
             hd.should.be.eql(require("os").homedir());
@@ -28,11 +28,11 @@ describe("homedir", function() {
             result.trim().should.be.eql(process.env.HOME + "\n/home/test");
         }
     });
-    
+
     it("should equal to another user with -i", function() {
         if(process.env.TRAVIS) {
             var syncRunner = require("sync-runner");
-            var result = syncRunner("sudo -u test -i " + process.execPath + " test_script.js", __dirname);
+            var result = syncRunner("sudo -u test -i " + process.execPath + " " + __dirname + "/test_script.js");
             result.trim().should.be.eql("/home/test\n/home/test");
         }
     });
